8749aa8211917cc8c89106b30eeadddbb55ec475
Karthik M A M <karthik_m_a_m@outlook.com>
2015-08-11 11:27:12 +0530

    Initial Commit

---
+++ GestureRecognition/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GestureRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GestureRecognition gestures = new GestureRecognition(Thread.CurrentThread, "COM5");
+            gestures.sampler.ProgressChanged += sampler_ProgressChanged;
+
+            Console.ReadKey();
+        }
+
+        static void sampler_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            try
+            {
+                switch (e.ProgressPercentage)
+                {
+                    case 0:
+                        Console.WriteLine("NEUTRAL");
+                        break;
+                    case 1:
+                        Console.WriteLine("LEFT");
+                        break;
+                    case 2:
+                        Console.WriteLine("RIGHT");
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
